87ce362c1958f9f8d571e4c16f7c9e0b898c6124
Bhargavinayak@13 <banavathubhargavi9@gmail.com>
2026-05-04 11:23:57 +0530

    fix port for render

--- app.py
+++ app.py
@@ -348,4 +348,5 @@ def delete_opportunity(opp_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+  
+  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
